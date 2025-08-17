7fc1e490c35936357c99ae602f59abf12b3a9686
theodan11 <theophiljadango@gmail.com>
2025-08-17 22:19:39 +0600

    addtocart

--- lib/core/screen/product/product_detail_page.dart
+++ lib/core/screen/product/product_detail_page.dart
@@ -1,4 +1,7 @@
 import 'package:e_commerce_app/core/common/header_and_see_all.dart';
+import 'package:e_commerce_app/core/common/product_card.dart';
+import 'package:e_commerce_app/core/dummyData/dummy_product_list.dart';
+import 'package:e_commerce_app/core/dummyData/product.dart';
 import 'package:e_commerce_app/core/theme/my_text_theme.dart';
 import 'package:e_commerce_app/core/theme/my_theme_colors.dart';
 import 'package:flutter/material.dart';
@@ -6,9 +9,9 @@ import 'package:money_formatter/money_formatter.dart';
 
 class ProductDetailPage extends StatelessWidget {
   const ProductDetailPage({super.key});
-
   @override
   Widget build(BuildContext context) {
+    final List<Product> products = DummyProducts.productList;
     MoneyFormatter money = MoneyFormatter(amount: 1500000);
     return Scaffold(
       appBar: AppBar(
@@ -172,6 +175,116 @@ class ProductDetailPage extends StatelessWidget {
                 height: 20,
                 color: MyThemeColors.grayText,
               ),
+              HeaderAndSeeAll(
+                headerTitle: "Reviews(86)",
+                btnTitle: "4.6",
+              ),
+              GestureDetector(
+                onTap: () {},
+                child: Container(
+                  height: 50,
+                  width: MediaQuery.of(context).size.width,
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10),
+                      border: Border.all(color: MyThemeColors.textNaviColor)),
+                  margin: const EdgeInsets.symmetric(
+                    horizontal: 15,
+                    vertical: 30,
+                  ),
+                  child: Align(
+                      alignment: Alignment.center,
+                      child: Text(
+                        "See All News",
+                        style: MyTextTheme.productTitle,
+                      )),
+                ),
+              ),
+              const HeaderAndSeeAll(
+                headerTitle: "Featured Product",
+              ),
+              const SizedBox(
+                height: 24,
+              ),
+              Container(
+                margin: const EdgeInsets.symmetric(horizontal: 25),
+                height: 280,
+                child: ListView.builder(
+                  shrinkWrap: true,
+                  scrollDirection: Axis.horizontal,
+                  itemCount: products.length,
+                  itemBuilder: (context, index) {
+                    Product productItem = products[index];
+
+                    return ProductCard(
+                      title: productItem.title,
+                      price: productItem.price,
+                      imgPath: productItem.img,
+                      rating: productItem.rating,
+                      numOfReviews: productItem.numOfReviews,
+                    );
+                  },
+                ),
+              ),
+              const SizedBox(
+                height: 36,
+              ),
+              Row(
+                children: [
+                  Expanded(
+                    child: GestureDetector(
+                      onTap: () {},
+                      child: Container(
+                        width: MediaQuery.of(context).size.width,
+                        height: 50,
+                        decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(10),
+                            color: MyThemeColors.productPriceColor),
+                        child: Padding(
+                          padding: const EdgeInsets.symmetric(
+                              horizontal: 20, vertical: 15),
+                          child: Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                Text(
+                                  "Added to card",
+                                  style: MyTextTheme.searchHintText.copyWith(
+                                    color: Colors.white,
+                                  ),
+                                ),
+                                const Icon(
+                                  Icons.favorite,
+                                  color: Colors.white,
+                                )
+                              ]),
+                        ),
+                      ),
+                    ),
+                  ),
+                  const SizedBox(
+                    width: 20,
+                  ),
+                  Expanded(
+                    child: GestureDetector(
+                      onTap: () {},
+                      child: Container(
+                        width: MediaQuery.of(context).size.width,
+                        height: 50,
+                        decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(10),
+                            color: MyThemeColors.primaryColor),
+                        child: Center(
+                          child: Text(
+                            "Add to Cart",
+                            style: MyTextTheme.searchHintText.copyWith(
+                              color: Colors.white,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                  )
+                ],
+              )
             ],
           ),
         ),
