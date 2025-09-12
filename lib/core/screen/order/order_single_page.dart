393b0b03382ee2424b654ece75405e2d72b687f8
theodan11 <theophiljadango@gmail.com>
2025-09-12 20:50:36 +0600

    bug fix

--- lib/core/screen/order/order_single_page.dart
+++ lib/core/screen/order/order_single_page.dart
@@ -18,111 +18,111 @@ class OrderSinglePage extends StatelessWidget {
       body: SafeArea(
         child: Padding(
           padding: const EdgeInsets.all(25.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text(
-                "Order ID: ${order.orderId}",
-                style: MyTextTheme.latestNewsHeadterText,
-              ),
-              const SizedBox(
-                height: 8,
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Text("Order Status:",
-                      style: MyTextTheme.latestNewsSubTitleText),
-                  Text(" ${order.status}",
-                      style: TextStyle(
-                        color: order.status == 'PENDING'
-                            ? MyThemeColors.primaryColor
-                            : order.status == "CANCELED"
-                                ? MyThemeColors.productPriceColor
-                                : MyThemeColors.greenCardColor,
-                      )),
-                ],
-              ),
-              const Divider(
-                height: 60,
-                color: MyThemeColors.grayText,
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Text(
-                    "Product Name",
-                    style: MyTextTheme.latestNewsHeadterText,
-                  ),
-                  const Spacer(),
-                  Text(
-                    "Quantity",
-                    style: MyTextTheme.latestNewsHeadterText,
-                  ),
-                  const SizedBox(
-                    width: 16,
-                  ),
-                  Text(
-                    "Price",
-                    style: MyTextTheme.latestNewsHeadterText,
-                  )
-                ],
-              ),
-              const Divider(
-                height: 40,
-                color: MyThemeColors.grayText,
-              ),
-              SizedBox(
-                height: MediaQuery.of(context).size.height * 0.48,
-                child: ListView.separated(
-                    separatorBuilder: (context, index) => const SizedBox(
-                          height: 14,
-                        ),
-                    itemCount: order.items.length,
-                    itemBuilder: (context, index) {
-                      var orderItem = order.items[index];
-                      return Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          ConstrainedBox(
-                            constraints: const BoxConstraints(maxWidth: 200),
-                            child: Flexible(
+          child: SingleChildScrollView(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  "Order ID: ${order.orderId}",
+                  style: MyTextTheme.latestNewsHeadterText,
+                ),
+                const SizedBox(
+                  height: 8,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text("Order Status:",
+                        style: MyTextTheme.latestNewsSubTitleText),
+                    Text(" ${order.status}",
+                        style: TextStyle(
+                          color: order.status == 'PENDING'
+                              ? MyThemeColors.primaryColor
+                              : order.status == "CANCELED"
+                                  ? MyThemeColors.productPriceColor
+                                  : MyThemeColors.greenCardColor,
+                        )),
+                  ],
+                ),
+                const Divider(
+                  height: 60,
+                  color: MyThemeColors.grayText,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      "Product Name",
+                      style: MyTextTheme.latestNewsHeadterText,
+                    ),
+                    const Spacer(),
+                    Text(
+                      "Quantity",
+                      style: MyTextTheme.latestNewsHeadterText,
+                    ),
+                    const SizedBox(
+                      width: 16,
+                    ),
+                    Text(
+                      "Price",
+                      style: MyTextTheme.latestNewsHeadterText,
+                    )
+                  ],
+                ),
+                const Divider(
+                  height: 40,
+                  color: MyThemeColors.grayText,
+                ),
+                SizedBox(
+                  height: MediaQuery.of(context).size.height * 0.48,
+                  child: ListView.separated(
+                      separatorBuilder: (context, index) => const SizedBox(
+                            height: 14,
+                          ),
+                      itemCount: order.items.length,
+                      itemBuilder: (context, index) {
+                        var orderItem = order.items[index];
+                        return Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            ConstrainedBox(
+                              constraints: const BoxConstraints(maxWidth: 200),
                               child: Text(
                                 orderItem["product"]["productTitle"],
                               ),
                             ),
-                          ),
-                          const Spacer(),
-                          Text(
-                            "x ${orderItem["quantity"].toString()}",
-                          ),
-                          const SizedBox(
-                            width: 10,
-                          ),
-                          Text(
-                            "৳ ${orderItem["product"]["productPrice"].toString()} tk",
-                          ),
-                        ],
-                      );
-                    }),
-              ),
-              const Divider(
-                color: MyThemeColors.grayText,
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Text(
-                    "Total Price",
-                    style: MyTextTheme.headerTextStyle,
-                  ),
-                  Text(
-                    "৳ ${order.totalPrice.toString()} tk",
-                    style: MyTextTheme.headerTextStyle,
-                  )
-                ],
-              )
-            ],
+                            const Spacer(),
+                            Text(
+                              "x ${orderItem["quantity"].toString()}",
+                            ),
+                            const SizedBox(
+                              width: 10,
+                            ),
+                            Text(
+                              "৳ ${orderItem["product"]["productPrice"].toString()} tk",
+                            ),
+                          ],
+                        );
+                      }),
+                ),
+                const Divider(
+                  color: MyThemeColors.grayText,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      "Total Price",
+                      style: MyTextTheme.headerTextStyle,
+                    ),
+                    Text(
+                      "৳ ${order.totalPrice.toString()} tk",
+                      style: MyTextTheme.headerTextStyle,
+                    )
+                  ],
+                )
+              ],
+            ),
           ),
         ),
       ),
